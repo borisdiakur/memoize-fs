c966c6886657bb367856eec1a3690d7be6caad7a
Boris Diakur <boris.diakur@medesso.de>
2014-06-20 10:10:07 +0200

    testing pre commit hook

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = function (options) {
                 }
             });
         });
-    }asdfasdf
+    }
 
     function checkOptions(optExt) {
         if (optExt.salt && typeof optExt.salt !== 'string') { throw new Error('salt option of type string expected, got \'' + typeof optExt.salt + '\''); }
