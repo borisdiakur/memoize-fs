6bac9e12f17ec91461f2f203aa73607a5a8da0c1
Boris Diakur <borisdiakur@gmail.com>
2025-03-08 10:48:11 +0100

    chore(dev-deps): bump eslint and vitest to latest versions

---
+++ eslint.config.mjs
@@ -0,0 +1,55 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      },
+      globals: {
+        ...globals.node,
+        ...globals.mocha
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/ban-ts-ignore': 'off',
+      'prefer-rest-params': 'off',
+      'prefer-spread': 'off',
+      'prettier/prettier': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ]
+    }
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module'
+    }
+  },
+  {
+    ignores: ['node_modules/**', 'coverage', 'dist']
+  }
+]
